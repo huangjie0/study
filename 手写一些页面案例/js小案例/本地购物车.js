2b6a68355b75350ff52c11468351b5962169f639
huangjie0 <1364045672@qq.com>
2022-03-03 11:17:14 +0800

    优化

--- 手写一些页面案例/js小案例/本地购物车.js
+++ 手写一些页面案例/js小案例/本地购物车.js
@@ -1,12 +1,11 @@
 var arr=[
-    {id:001,buy_num:1,number:15,select:true,name:'2020宿州国际音乐节',city:'宿州',price:150,time:'2020.01.24',address:'https://img1.baidu.com/it/u=436907149,1086251211&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=775'},
-    {id:002,buy_num:1,number:15,select:false,name:'2020南京国际音乐节',city:'南京',price:200,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F010e735dbea026a801209e1f17a3f9.jpg%401280w_1l_2o_100sh.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=755ae4419bbe140978c844f34114dc70'},
-    {id:003,buy_num:5,number:15,select:true,name:'2020澳门国际音乐节',city:'澳门',price:130,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp9.itc.cn%2Fq_70%2Fimages03%2F20210715%2F0fc7dda433ce4b30ac2bd42050f9b8af.jpeg&refer=http%3A%2F%2Fp9.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241552&t=c3738f72fb5a8e6499560ba28577bb4b'},
-    {id:004,buy_num:1,number:15,select:true,name:'2020香港国际音乐节',city:'香港',price:125,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.5tu.cn%2Fattachments%2Fimage_files%2Fmonth_1908%2F201907272208394797.jpg&refer=http%3A%2F%2Fimg.5tu.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=02515e3fd438998e2f11fd7554d8bd65'},
-    {id:005,buy_num:1,number:15,select:true,name:'2020芜湖国际音乐节',city:'芜湖',price:136,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fbackgd%2Fcover%2F00%2F20%2F00%2F5b96151272466.jpg%21%2Ffw%2F780%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=172cf09834f1e4c6b67fc8d3b794c833'},
+    {id:01,buy_num:1,number:15,select:true,name:'2020宿州国际音乐节',city:'宿州',price:150,time:'2020.01.24',address:'https://img1.baidu.com/it/u=436907149,1086251211&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=775'},
+    {id:02,buy_num:1,number:15,select:false,name:'2020南京国际音乐节',city:'南京',price:200,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F010e735dbea026a801209e1f17a3f9.jpg%401280w_1l_2o_100sh.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=755ae4419bbe140978c844f34114dc70'},
+    {id:03,buy_num:5,number:15,select:true,name:'2020澳门国际音乐节',city:'澳门',price:130,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp9.itc.cn%2Fq_70%2Fimages03%2F20210715%2F0fc7dda433ce4b30ac2bd42050f9b8af.jpeg&refer=http%3A%2F%2Fp9.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241552&t=c3738f72fb5a8e6499560ba28577bb4b'},
+    {id:04,buy_num:1,number:15,select:true,name:'2020香港国际音乐节',city:'香港',price:125,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.5tu.cn%2Fattachments%2Fimage_files%2Fmonth_1908%2F201907272208394797.jpg&refer=http%3A%2F%2Fimg.5tu.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=02515e3fd438998e2f11fd7554d8bd65'},
+    {id:05,buy_num:1,number:15,select:true,name:'2020芜湖国际音乐节',city:'芜湖',price:136,time:'2020.01.24',address:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fbackgd%2Fcover%2F00%2F20%2F00%2F5b96151272466.jpg%21%2Ffw%2F780%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647241660&t=172cf09834f1e4c6b67fc8d3b794c833'},
     ]
 var content = document.querySelector('.content');
-
 bindHtml()
 function bindHtml(){
     var totalPrice = 0,totalNum=0,selectNum=0
@@ -56,62 +55,63 @@ function bindHtml(){
         </di
     `
     content.innerHTML=str; 
-
-    content.addEventListener('click',function(e){
-        e=e||window.event
-        var target = e.target || e.srcElement
-        if(target.className==='selectAll'){
-            var target_checked = target.checked
-            arr.forEach(function(item){
-                item.select=target_checked
-            })
-            bindHtml()
-        }
-        if(target.className==='list'){
-            var id = target.dataset.id-0
-            var goods = arr.find(function(item){
-                return item.id===id
-            })
-            goods.select=!goods.select
-            bindHtml()
-        }
-        if(target.className==='sub'){
-            var id = target.dataset.id-0
-            var goods = arr.find(function(item){
-                return item.id===id
-            })
-            if(goods.buy_num<=1)return
-            goods.buy_num--
-            bindHtml()
-        }
-        if(target.className==='add'){
-            var id = target.dataset.id-0
-            var goods = arr.find(function(item){
-                return item.id==id
-            })
-            if(goods.buy_num>=goods.number)return
-            goods.buy_num++
-            bindHtml()
-        }
-        if(target.className==='del'){
-            var id =target.dataset.id-0
-            arr=arr.filter(function(item){
-                return item.id!==id
-            })
-            bindHtml()
-        }
-        if(target.className==='qkgwc'){
-           arr=[]
-           bindHtml()
-        }
-        if(target.className==='scxz'){
-            arr=arr.filter(function(item){
-                return item.select===false
-            })
-            bindHtml()
-        }
-        if(target.className==='fhlb'){
-           window.location.href='https://www.jd.com'
-        }
-    })
 }
+content.addEventListener('click',function(e){
+    console.log(111)
+    e=e||window.event
+    var target = e.target || e.srcElement
+    if(target.className==='selectAll'){
+        var target_checked = target.checked
+        arr.forEach(function(item){
+            item.select=target_checked
+        })
+        bindHtml()
+    }
+    if(target.className==='list'){
+        var id = target.dataset.id-0
+        var goods = arr.find(function(item){
+            return item.id===id
+        })
+        goods.select=!goods.select
+        bindHtml()
+    }
+    if(target.className==='sub'){
+        var id = target.dataset.id-0
+        var goods = arr.find(function(item){
+            return item.id===id
+        })
+        if(goods.buy_num<=1)return
+        goods.buy_num--
+        bindHtml()
+    }
+    if(target.className==='add'){
+        var id = target.dataset.id-0
+        var goods = arr.find(function(item){
+            return item.id==id
+        })
+        if(goods.buy_num>=goods.number)return
+        console.log(goods)
+        goods.buy_num++
+        bindHtml()
+    }
+    if(target.className==='del'){
+        var id =target.dataset.id-0
+        arr=arr.filter(function(item){
+            return item.id!==id
+        })
+        bindHtml()
+    }
+    if(target.className==='qkgwc'){
+       arr=[]
+       bindHtml()
+    }
+    if(target.className==='scxz'){
+        arr=arr.filter(function(item){
+            return item.select===false
+        })
+        bindHtml()
+    }
+    if(target.className==='fhlb'){
+       window.location.href='https://www.jd.com'
+    }
+})
