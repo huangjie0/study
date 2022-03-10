acd2f2312417e6aa6a229dbbb343c67d44327913
huangjie0 <1364045672@qq.com>
2022-03-10 15:38:13 +0800

    作业

--- 手写一些页面案例/题目/js/项目练习/轮播图.js
+++ 手写一些页面案例/题目/js/项目练习/轮播图.js
@@ -1,65 +1,26 @@
-//获取左右按钮标签
-var left = document.querySelector('.left');
-var right = document.querySelector('.right');
+//获取所有要操作的标签
 var ul = document.querySelector('.ul');
 var ol =document.querySelector('ol');
-var box = document.querySelector('.box');
-//封装轮播图函数
-var i = 1;
+var box = document.querySelector('.box')
+
+//封装运动函数
+var i=1;
 function move(){
     i++;
     ul.style.transform = "translateX(" + (-600*(i-1)) + "px)";
-    if(i===5){
-        i=1;
-        ul.style.transform = "translateX(" + (600*(i-1)) + "px)";  
+    if(i==6){
+        i=2;
+        ul.style.transform = "translateX(" + (600*(i-3)) + "px)";
     }
     for(var j=0;j<ol.children.length;j++){
-        ol.children[j].classList.remove('active');
+        ol.children[j].classList.remove('active')
     }
     ol.children[i-2].classList.add('active');
 }
-//封装定时器函数
-var move1 = setInterval(
-    function(){
-        move();
-    },2000);
-//点击按钮事件
-function click (){
-    box.addEventListener('click',function(e){
-        e=e||window.event;
-        var target = e.target||e.srcElement
-        if(target.className=='left'){
-            i--;
-            ul.style.transform = "translateX(" + (-600*(i-1)) + "px)";
-            if(i===0){
-                i=4;
-                ul.style.transform = "translateX(" + (600*i) + "px)";  
-            }
-            for(var j=0;j<ol.children.length;j++){
-            ol.children[j].classList.remove('active');
-            }
-            ol.children[i-1].classList.add('active');
-        }
-        }
-    )
-    box.addEventListener('click',function(e){
-    e=e||window.event;
-    var target = e.target||e.srcElement
-    if(target.className=='right'){
-        ul.style.transform = "translateX(" + (-600*i) + "px)";
-        i++;
-        if(i===5){
-            i=1;
-            ul.style.transform = "translateX(" + (600*(i-1)) + "px)";  
-        }
-        for(var j=0;j<ol.children.length;j++){
-            ol.children[j].classList.remove('active');
-        }
-        ol.children[i-1].classList.add('active');
-    }
-    })
-}
-click();
+//定时器函数封装
+var move1 = setInterval(function(){
+    move()
+},2000);
 //鼠标移入移出事件
 box.onmouseover=function(){
     clearInterval(move1);
@@ -70,7 +31,31 @@ box.onmouseout=function(){
     },2000)
 }
 
-
+//点击事件
+box.addEventListener('click',function(e){
+    e=e||window.event;
+    var target = e.target || e.srcElement;
+    if(target.className=='left'){
+        ul.style.transform = "translateX(" + (-600*(i-1)) + "px)";
+        i--;
+        if(i==0){
+            i=4;
+            ul.style.transform = "translateX(" + (-2400) + "px)";
+        }
+    }
+})
+box.addEventListener('click',function(e){
+    e=e||window.event;
+    var target = e.target || e.srcElement;
+    if(target.className=='right'){
+        ul.style.transform = "translateX(" + (-600*i) + "px)";
+        i++;
+        if(i==5){
+            i=1;
+            ul.style.transform = "translateX(" + (-600*(i-1)) + "px)";
+        }
+    }
+})
 
 
 
