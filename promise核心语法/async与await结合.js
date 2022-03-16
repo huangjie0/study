a684c89d65bf1ed0529e6095b734af06c83db925
huangjie0 <1364045672@qq.com>
2022-03-16 14:34:34 +0800

    作业

---
+++ promise核心语法/async与await结合.js
@@ -0,0 +1,16 @@
+//导入fs模块
+const fs = require('fs');
+//导入util
+const util_1 = require('util');
+//利用util进行包装
+const myUtil = util_1.promisify(fs.readFile);
+
+async function main(){
+    try{
+        
+        console.log(await (await myUtil('./1.txt')).toString())
+    }catch(e){
+        console.log(e);
+    }
+}
+main();
