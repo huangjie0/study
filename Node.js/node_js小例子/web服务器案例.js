fc21ed40f18098c69b2c658ce001cf88d849d6ce
huangjie0 <1364045672@qq.com>
2022-03-16 22:06:42 +0800

    zyo

--- Node.js/node_js小例子/web服务器案例.js
+++ Node.js/node_js小例子/web服务器案例.js
@@ -13,10 +13,9 @@ server.on('request',(req,res)=>{
     const fpath=path.join(__dirname,url);
     fs.readFile(fpath,'utf-8',(err,dataStr)=>{
         if(err) return res.end('读取失败'+err)
-        res.end(dataStr);
+        res.end('读取成功'+dataStr);
     })
     res.setHeader('Content-Type','text/html;charset=utf-8');
-
 })
 //启动服务器
 server.listen(80,()=>{
