c60bba0dcbf9daa7b754613e26c5d211a7b1e954
huangjie0 <1364045672@qq.com>
2022-03-16 20:50:46 +0800

    ，，

---
+++ Node.js/node_js小例子/web服务器案例.js
@@ -0,0 +1,24 @@
+//实现步骤
+//导入http模块
+const http = require('http');
+//导入fs模块
+const fs=require('fs');
+//导入path模块
+const path = require('path');
+//创建基本的web服务器  
+const server = http.createServer();
+//监听web服务器的request事件
+server.on('request',(req,res)=>{
+    const url = req.url;
+    const fpath=path.join(__dirname,url);
+    fs.readFile(fpath,'utf-8',(err,dataStr)=>{
+        if(err) return res.end('读取失败'+err)
+        res.end(dataStr);
+    })
+    res.setHeader('Content-Type','text/html;charset=utf-8');
+
+})
+//启动服务器
+server.listen(80,()=>{
+    console.log('server running at http://127.0.0.1')
+})
