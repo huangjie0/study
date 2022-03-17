5056e0630b0db59d77fbbe2c4eff66a73a5db210
huangjie0 <1364045672@qq.com>
2022-03-17 14:45:53 +0800

    作业

---
+++ Node.js/node_js小例子/使用npm进行格式化时间.js
@@ -0,0 +1,7 @@
+//导入需要的包
+const moment = require('moment');
+//使用moment里面的格式化时间的方法
+const dt = moment().format('YYYY-MM-DD HH:mm:ss');
+console.log(dt);
+//当安装npm下包的时候，文件会多了node_modules这个文件和package-lock.json文件的配置文件
+
