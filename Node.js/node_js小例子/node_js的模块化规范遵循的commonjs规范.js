704e0db15df5508afb78247f1fc8ad43021ef899
huangjie0 <1364045672@qq.com>
2022-03-17 11:35:09 +0800

    作业

--- Node.js/node_js小例子/node_js的模块化规范遵循的commonjs规范.js
+++ Node.js/node_js小例子/node_js的模块化规范遵循的commonjs规范.js
@@ -1,2 +1,3 @@
 //commonjs规范规定的特性和各个模块之间如何相互依赖。
 //commonjs规定：每个模块内部，module变量代表当前模块
+//npm使用npm -v来查看npm的版本号
---
+++ Node.js/node_js小例子/格式化时间测试.js
@@ -0,0 +1,2 @@
+const Time = require('./格式化实现');
+console.log(Time.dataFormat(new Date()));
