5c1cc0e60b094b6f7d07653d290da37a1eb033e1
huangjie0 <1364045672@qq.com>
2022-03-18 11:44:48 +0800

    练习

--- Node.js/node_js小例子/test.js
+++ Node.js/node_js小例子/test.js
@@ -3,6 +3,10 @@ const test = require('../HJTest/index');
 // const dtStr = test.dateFormat(new Date());
 // console.log(dtStr);
 //测试html转义
-const htmlStr = '<h1>首页</h1>'
-const result = test.htmlEscape(htmlStr);
-console.log(result);
+// const htmlStr = '<h1>首页</h1>'
+// const result = test.htmlEscape(htmlStr);
+// console.log(result);
+//测试转义html
+const Str = '&lt;h1&gt;首页&lt;h1&gt;'
+const str = test.StrEscape(Str);
+console.log(str);
