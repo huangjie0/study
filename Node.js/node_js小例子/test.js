3b4cb39b33eaf3b8da2f77bf2a793c2a44352d0f
huangjie0 <1364045672@qq.com>
2022-03-18 11:12:43 +0800

    练习

--- Node.js/node_js小例子/test.js
+++ Node.js/node_js小例子/test.js
@@ -1,4 +1,8 @@
 const test = require('../HJTest/index');
 //格式化时间的功能
-const dtStr = test.dateFormat(new Date());
-console.log(dtStr);
+// const dtStr = test.dateFormat(new Date());
+// console.log(dtStr);
+//测试html转义
+const htmlStr = '<h1>首页</h1>'
+const result = test.htmlEscape(htmlStr);
+console.log(result);
