c0578efc85d4fbcdd5921ed0170c38479dd382ba
huangjie0 <1364045672@qq.com>
2022-03-19 17:47:47 +0800

    优化

---
+++ Node.js/node_js小例子/express托管静态资源.js
@@ -0,0 +1,10 @@
+//导入express库
+const express = require('express');
+//实例化对象
+const app = express();
+//调用静态方法，快速对外提供静态资源
+app.use(express.static('./File'));
+//启动服务器
+app.listen(80,()=>{
+    console.log('服务器启动成功!,端口号是http://127.0.0.1')
+})
