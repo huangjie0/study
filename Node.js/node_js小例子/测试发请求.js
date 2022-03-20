ded7a15536fdff479c4970b416eea97fad8fd2bc
huangjie0 <1364045672@qq.com>
2022-03-20 23:58:14 +0800

    优化

--- Node.js/node_js小例子/测试发请求.js
+++ Node.js/node_js小例子/测试发请求.js
@@ -4,12 +4,22 @@ const express = require('express');
 const app = express();
 //创建路由
 app.get('./发请求测试.html',(req,res)=>{
-    //设置响应头，设置允许跨域
-    res.setHeader('Access-Control-Allow-Origin','*');
     //设置响应体
     res.send('Hello ajax');
 })
+//解决跨域问题
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
 //开启服务器
 app.listen(8000,()=>{
     console.log('服务器已经启动了，8000端口监听中.....')
 })
+
+
+
+
