dfd75dba6fefbb83a787289ed4e60fbac05f8059
huangjie0 <1364045672@qq.com>
2022-03-21 09:35:02 +0800

    优化

---
+++ Node.js/node_js小例子/express全局中间件.js
@@ -0,0 +1,23 @@
+//导入express模块
+const express = require('express');
+//创建express实例
+const app = express();
+//注册全局中间件,中间件作用，多个中间件共享一个req,res，
+// 基于这样的特征，我们可以在上游中间件，统一为res,req添加属性和方法，在上游添加下游访问
+app.use(function(req,res,next){
+    console.log('这是最简单的中间件');
+    next();
+});
+//定义路由
+app.get('/',(req,res)=>{
+    console.log('这是/路由');
+    res.send('hello world')
+})
+app.get('/user',(req,res)=>{
+    console.log('这是/user路由');
+    res.send('hello world,你好世界');
+})
+//启动服务器
+app.listen(80,()=>{
+    console.log('http://127.0.0.1');
+})
