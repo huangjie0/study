0940fcfd766acf746f44fa942c38a4f34ddb2fb1
huangjie0 <1364045672@qq.com>
2022-03-21 09:44:48 +0800

    优化

--- Node.js/node_js小例子/测试发请求.js
+++ Node.js/node_js小例子/测试发请求.js
@@ -2,19 +2,19 @@
 const express = require('express');
 //创建服务器对象
 const app = express();
+//解决跨域问题
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method' )
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE')
+    res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE')
+    next();
+})
 //创建路由
-app.get('./发请求测试.html',(req,res)=>{
+app.get('./testget',(req,res)=>{
     //设置响应体
     res.send('Hello ajax');
 })
-//解决跨域问题
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    next();
-});
 //开启服务器
 app.listen(8000,()=>{
     console.log('服务器已经启动了，8000端口监听中.....')
