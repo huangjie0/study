e163b06108a7496821930571865149d77cf73119
huangjie0 <1364045672@qq.com>
2022-03-22 13:42:59 +0800

    优化

--- Node.js/node_js小例子/配置mysql模块.js
+++ Node.js/node_js小例子/配置mysql模块.js
@@ -8,7 +8,30 @@ const db = mysql.createPool({
     database:'test'
 });
 //测试是否配置成功；
-db.query('select 1',(err,result)=>{
-    if(err) return console.log(err.message);
-    console.log(result); 
-});
+// db.query('select 1',(err,result)=>{
+//     if(err) return console.log(err.message);
+//     console.log(result); 
+// });
+//查询sql语句,如果执行select语句,得到的是数组。
+// 定义sql语句
+// const sql = 'select * from users';
+// db.query(sql,(err,result)=>{
+//     if(err)return err.message
+//     console.log(result);
+// });
+//插入数据对象
+// const user={username:'xiaoming',password:'30303030'};
+// const sql = 'insert into users (username,password) values (?,?)'
+// db.query(sql,[user.username,user.password],(err,result)=>{
+//     if(err)return console.log(err.message);
+//     if(result.affectRows===1){
+//         console.log('插入成功');
+//     }
+// })
+//删除数据
+// const sql = "DELETE FROM users WHERE username = 'xiaoming'"
+// db.query(sql,(err,result)=>{
+//     if(err)return console.log(err.message)
+//     console.log(result);
+// })
+
