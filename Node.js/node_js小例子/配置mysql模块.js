5859bede90604be7ef999f634490b27e160c030f
huangjie0 <1364045672@qq.com>
2022-03-22 14:12:30 +0800

    优化

--- Node.js/node_js小例子/配置mysql模块.js
+++ Node.js/node_js小例子/配置mysql模块.js
@@ -24,27 +24,37 @@ const db = mysql.createPool({
 // const sql = 'insert into users (username,password) values (?,?)'
 // db.query(sql,[user.username,user.password],(err,result)=>{
 //     if(err)return console.log(err.message);
-//     if(result.affectRows===1){
+//     if(result.affectedRows===1){
 //         console.log('插入成功');
 //     }
 // })
 //插入多条数据写法
-const user={username:'ll',password:'44444',
-            username:'pp',password:'99999',
-            username:'gg',password:'10121212',status:1,
-            username:'www',password:'886666'
-            }
-const sql = 'insert into users set ?'
-db.query(sql,user,(err,result)=>{
-    if(err)return console.log(err.message);
-    if(result.affectRows===1){
-        console.log('插入成功');
-    }
-})
+// const user={username:'ll',password:'44444',
+//             username:'pp',password:'99999',
+//             username:'gg',password:'10121212',status:1,
+//             username:'www',password:'886666',
+//             username:'mmm',password:'303030'
+//             }
+// const sql = 'insert into users set ?'
+// db.query(sql,user,(err,result)=>{
+//     if(err)return console.log(err.message);
+//     if(result.affectedRows===1){
+//         console.log('插入成功');
+//     }
+// })
 //删除数据
 // const sql = "DELETE FROM users WHERE username = 'xiaoming'"
 // db.query(sql,(err,result)=>{
 //     if(err)return console.log(err.message)
 //     console.log(result);
 // })
+//更新数据
+const user = {password:'9',id:2};
+const sql="update users set password=? where id=?"
+db.query(sql,[user.password,user.id],(err,result)=>{
+    if(err)return console.log(err.message)
+    if(result.affectedRows===1){
+        console.log('更新成功');
+    }
+})
 
