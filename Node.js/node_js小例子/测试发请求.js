a7159c636e57619c5943a1b4ece7508f3d5b3605
huangjie0 <1364045672@qq.com>
2022-03-23 10:22:16 +0800

    优化

--- Node.js/node_js小例子/测试发请求.js
+++ Node.js/node_js小例子/测试发请求.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
-// const mysql = require('mysql');
+const mysql = require('mysql');
 //创建服务器对象
 const app = express();
 var corsOptions = {
@@ -22,36 +22,34 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 //创建数据库
 //建立mysql与项目之间的联系
-// const db = mysql.createPool({
-//     host:'127.0.0.1',
-//     user:'root',
-//     password:'123456',
-//     database:'test'
-// });
+const db = mysql.createPool({
+    host:'127.0.0.1',
+    user:'root',
+    password:'123456',
+    database:'test'
+});
 //定义sql语句
-// const sql = 'select username,password from users'
-// db.query(sql,(err,result)=>{
-//     if(err)return console.log(err.message);
-//     result.forEach((item)=>{
-//         var username = item.username;
-//         var userpwd = item.password;
-//     })
-// })
+const sql = 'select username,password from users'
+db.query(sql,(err,result)=>{
+    if(err)return console.log(err.message);
+    result.forEach((item)=>{
+
+    })
+})
 //创建路由
 app.post('/testget',(req,res)=>{
     var query = req.query
-    var userName = query.userName;
-    var userPwd = query.userPwd;
-    if(userName!='huangjie'||userPwd!='123456'){
-        return res.send({status:1,msg:'登录失败'})
-    }
-    res.send({status:0,msg:'登录成功'})
+    var userName = query.userName
+    var userPwd = query.userPwd
+    if(userName=='huangjie'&&userPwd=='12345678'){ 
+        res.send({status:0,msg:'登录成功'});
+    }else{
+        res.send({status:1,msg:'登录失败'});
+    };
 })
 //开启服务器
 app.listen(8000,()=>{
     console.log('服务器已经启动了，8000端口监听中.....')
 });
-
-
 
 
