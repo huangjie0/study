73919fe330944efe6486785d920620ebd6f450ff
huangjie0 <1364045672@qq.com>
2022-03-23 14:55:51 +0800

    优化

--- Node.js/node_js小例子/session使用.js
+++ Node.js/node_js小例子/session使用.js
@@ -1,1 +1,21 @@
+//导入express模块
+const express = require('express');
+//创建express服务器实例
+
+const app = express();
+
+//配置session中间件
+const session = require('express-session');
+//注册session中间件
+app.use(session({
+    secret:'huangjie',
+    resave:false,
+    saveUninitialized:true,
+}));
+
+//托管静态页面
+app.use(express.static('./File'));
+//解析post提交过来的数据表单
+app.use(express.urlencoded({extended:false}));
+
 
