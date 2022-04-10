7c70b6a2bb8f8dbd18e0b467c2f5d2dccda14066
huangjie0 <1364045672@qq.com>
2022-04-10 22:13:44 +0800

    优化

--- vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/src/App.vue
+++ vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <Test/>
+        <Test2/>
     </div>
 </template>
 <script>
     import Test from '../src/components/Test.vue'
+    import Test2 from '../src/components/Test2.vue'
     export default {
         name:'App',
         components:{
-            Test
+            Test,
+            Test2
         }
     }
 </script>
---
+++ vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/src/components/Test2.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+      <button @click='isShow=!isShow'>显示与隐藏</button>
+      <transition-group name='hello' appear>
+          <h1 v-show='isShow' class="go" key="1">你好啊！</h1>
+          <h1 v-show='isShow' class="go" key="2">你好啊！</h1>
+      </transition-group>
+</div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1{
+    background-color: orange;
+}
+.hello-enter,.hello-leave-to{
+    transform: translateX(-100%);
+}
+.hello-enter-to,.hello-leave{
+    transform: translateX(0px);
+}
+.hello-enter-active,.hello-leave-active{
+    transition: 1s linear;
+}
+</style>
