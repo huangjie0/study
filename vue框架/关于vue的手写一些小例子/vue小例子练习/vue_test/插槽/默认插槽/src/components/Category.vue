a75802007e144ce01f296a83e224d3827624e9d7
huangjie0 <1364045672@qq.com>
2022-04-11 21:30:19 +0800

    优化

---
+++ vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/插槽/默认插槽/src/components/Category.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="category">
+    <h3>{{title}}分类</h3>
+    <slot></slot>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:'Category',
+    props:['title']
+
+}
+</script>
+
+<style>
+.category{
+    background-color: skyblue;
+    width: 200px;
+    height: 300px;
+}
+h3{
+    text-align: center;
+    background-color: orange;
+}
+
+</style>
