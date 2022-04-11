d493cd025b4be3d812ced1b4b82ebb607ee3e55b
huangjie0 <1364045672@qq.com>
2022-04-11 23:24:31 +0800

    优化

---
+++ vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/插槽/作用域插槽/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="div">
+    <Category title='美食'>
+        <template scope="games">
+            <ul>
+                <li v-for="(g,index) in games.games" :key="index">{{g}}</li>
+            </ul>
+        </template>
+    </Category>
+</div>
+</template>
+<script>
+import Category from './components/Category.vue'
+    export default {
+        name:'App',
+        components:{
+            Category
+        }
+    }
+</script>
+<style scoped>
+.div{
+    display: flex;
+    justify-content: space-around;
+
+}
+img{
+    width: 200px;
+    height: 200px;
+}
+</style>
---
+++ vue框架/关于vue的手写一些小例子/vue小例子练习/vue_test/插槽/作用域插槽/src/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="category">
+    <h3>{{title}}分类</h3>
+    <slot :games='games'></slot>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:'Category',
+    props:['title'],
+    data() {
+        return {
+            games:['qq飞车','lol','cf','超级玛丽']
+        }
+    },
+
+}
+</script>
+
+<style>
+.category{
+    background-color: skyblue;
+    width: 200px;
+    height: 300px;
+}
+h3{
+    text-align: center;
+    background-color: orange;
+}
+
+</style>
