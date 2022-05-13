54a38b5d5a5fbe637cde3516c9f9ca18e5b89a48
huangjie0 <1364045672@qq.com>
2022-05-13 11:06:41 +0800

    优化

--- 手写一些页面案例/题目/vue项目/改成组件形式/todolistrouter/src/router/index.js
+++ 手写一些页面案例/题目/vue项目/改成组件形式/todolistrouter/src/router/index.js
@@ -5,6 +5,7 @@ import Unfinished from '../views/Unfinished.vue'
 import Finished from '../views/Finished.vue'
 import All from '../views/All.vue'
 import Login from '../views/Login.vue'
+import Page_404 from '../views/Page_404.vue'
 Vue.use(VueRouter);
 
 //重写底层代码
@@ -46,12 +47,18 @@ const routers = new VueRouter({
                     name:'all'
                 }
             ]
+        },
+        {
+            path:'/*',
+            component:Page_404,
+            name:'page_404'
         }
     ]
 })
+//路由前置守卫
 routers.beforeEach((to,from,next)=>{
     if(to.path!=='/login'){
-        const user = window.localStorage.getItem('user')
+        const user = window.localStorage.getItem('user');
         if(user){
             next()
         }else{
@@ -61,6 +68,5 @@ routers.beforeEach((to,from,next)=>{
         next()
     }
 })
-
-
+//暴露函数
 export default routers
---
+++ 手写一些页面案例/题目/vue项目/改成组件形式/todolistrouter/src/views/Page_404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>
+          您访问的网页不存在
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Page_404'
+}
+</script>
+
+<style scoped>
+</style>
