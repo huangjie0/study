47fe198b54a855ac186cdc42beccacdc61f7dca4
huangjie0 <1364045672@qq.com>
2022-06-16 14:45:37 +0800

    优化

--- vue3核心语法/vue_3_test/src/App.vue
+++ vue3核心语法/vue_3_test/src/App.vue
@@ -2,19 +2,21 @@
 <!-- vue3组件中模板结构可以没有根标签 -->
 <div>
   <h1>一个人的信息</h1>
-  <h2>姓名:{{person.name}}</h2>
+  <!-- <h2>姓名:{{person.name}}</h2>
   <h2>年龄:{{person.age}}</h2>
   <h3>工作种类:{{person.job.type}}</h3>
   <h3>工作薪水:{{person.job.salary}}</h3>
-  <h3>爱好:{{person.hobby[0]}}</h3>
+  <h3>爱好:{{person.hobby[0]}}</h3> -->
+  <h3>爱好:{{hobby}}</h3>
   <button @click="changeName">点我修改姓名</button>
+  <Test msg='我是vue子组件' age='18'/>
 </div>
-<Test msg='我是vue子组件' age='18'/>
 </template>
 
 <script>
+import { reactive, ref } from '@vue/reactivity';
 //引入ref函数
-import {reactive} from 'vue'
+// import {reactive} from 'vue'
 import Test from '../src/components/Test'
 export default {
   name: 'App',
@@ -36,26 +38,28 @@ export default {
     //     }
     //   }
     // })
-    // let hobby=reactive(['抽烟','喝酒','烫头']);
-    let person=reactive({
-      name:'张三',
-      age:18,
-      job:{
-        type:'前端工程师',
-        salary:'30k'
-      },
-      hobby:['抽烟','喝酒','烫头']
-    })
+    let hobby=reactive(['抽烟','喝酒','烫头'])
+    // let person=reactive({
+    //   name:'张三',
+    //   age:18,
+    //   job:{
+    //     type:'前端工程师',
+    //     salary:'30k'
+    //   },
+    //   hobby:['抽烟','喝酒','烫头']
+    // })
     //配置一个方法
     // function sayHello(){
     //   alert(`我加${name},我是${age}岁了!`)
     // }
     function changeName(){
-      person.name='李四'
+      // person.name='李四'
+      hobby[0]='jiehhhh'
     }
     //函数靠一个返回值
     return {
-      person,
+      // person,
+      hobby,
       changeName
     }
     //返回是一个渲染函数
