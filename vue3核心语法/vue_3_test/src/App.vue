751499fb1bf3230415ab85b703a60a614beddc86
huangjie0 <1364045672@qq.com>
2022-06-16 10:55:23 +0800

    优化

--- vue3核心语法/vue_3_test/src/App.vue
+++ vue3核心语法/vue_3_test/src/App.vue
@@ -2,60 +2,58 @@
 <!-- vue3组件中模板结构可以没有根标签 -->
 <div>
   <h1>一个人的信息</h1>
-  <h2>姓名:{{name}}</h2>
-  <h2>年龄:{{age}}</h2>
-  <h3>工作种类:{{job.type}}</h3>
-  <h3>工作薪水:{{job.salary}}</h3>
-  <h3>爱好:{{hobby[0]}}</h3>
-  <h3>测试的数据:{{job.a.b.c}}</h3>
-  <button @click="changeUserinfo()">修改人物信息</button>
+  <h2>姓名:{{person.name}}</h2>
+  <h2>年龄:{{person.age}}</h2>
+  <h3>工作种类:{{person.job.type}}</h3>
+  <h3>工作薪水:{{person.job.salary}}</h3>
+  <h3>爱好:{{person.hobby[0]}}</h3>
+  <button @click="changeName">点我修改姓名</button>
 </div>
 </template>
 
 <script>
 //引入ref函数
-import {reactive,ref} from 'vue'
+import {reactive} from 'vue'
 export default {
   name: 'App',
   //配置数据
   setup() {
     //变成响应式
-    let name=ref('张三')
-    let age= ref(18)
-    let job = reactive({
-      type:'前端工程师',
-      salary:'30k',
-      a:{
-        b:{
-          c:'111'
-        }
-      }
+    // let name=ref('张三')
+    // let age= ref(18)
+    // let job = reactive({
+    //   type:'前端工程师',
+    //   salary:'30k',
+    //   a:{
+    //     b:{
+    //       c:'111'
+    //     }
+    //   }
+    // })
+    // let hobby=reactive(['抽烟','喝酒','烫头']);
+    let person=reactive({
+      name:'张三',
+      age:18,
+      job:{
+        type:'前端工程师',
+        salary:'30k'
+      },
+      hobby:['抽烟','喝酒','烫头']
     })
-    let hobby=['抽烟','喝酒','烫头']
     //配置一个方法
     // function sayHello(){
     //   alert(`我加${name},我是${age}岁了!`)
     // }
-    function changeUserinfo(){
-      console.log(name.value)
-      console.log(age.value)
-      // job.value.type='ui设计师'
-      console.log(job.type)
-      console.log(job.salary)
-      console.log(job.a.b.c)
-      job.a.b.c=999
-      hobby[0]='学习'
+    function changeName(){
+      person.name='李四'
     }
     //函数靠一个返回值
     return {
-      name,
-      age,
-      changeUserinfo,
-      job,
-      hobby
+      person,
+      changeName
     }
     //返回是一个渲染函数
-    // return ()=>{
+    // return ()=>{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     //   return h('h1','黄石')
     // }
   }
