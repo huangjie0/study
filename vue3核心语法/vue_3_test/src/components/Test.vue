f58fb74b27abc4fced74438963e2346b1cabb6d4
huangjie0 <1364045672@qq.com>
2022-06-17 14:48:09 +0800

    优化

--- vue3核心语法/vue_3_test/src/components/Test.vue
+++ vue3核心语法/vue_3_test/src/components/Test.vue
@@ -6,9 +6,6 @@
       <h2>
           {{msg}}
       </h2>
-      <h2>
-          {{person.a.b}}
-      </h2>
       <button @click="msg+='!'">
           修改信息
       </button>
@@ -23,14 +20,11 @@
       <button @click="chanegAge()">
           增长年龄
       </button>
-      <button @click="listen()">
-          监听对象某个属性
-      </button>
   </div>
 </template>
 
 <script>
-import {reactive,ref,watch} from 'vue'
+import {reactive,ref,watchEffect} from 'vue'
 export default {
     name:'Test',
     setup() {
@@ -39,8 +33,10 @@ export default {
         let person = reactive({
             name:'张三',
             age:18,
-            a:{
-                b:1
+            job:{
+                j1:{
+                    salary:20
+                }
             }
         })
         //情况一，监视ref的一个响应式数据
@@ -58,9 +54,6 @@ export default {
         function chanegAge(){
             person.age += 1
         }
-        function listen(){
-            person.a.b++
-        }
         //情况二，监视ref的多个响应式数据
         // watch([sum,msg],(newval,oldval)=>{
         //     console.log('sum的值变化了',newval,oldval)
@@ -69,17 +62,19 @@ export default {
         // watch(person,(newvalue,oldvalue)=>{
         //     console.log('person变化了',newvalue,oldvalue)
         // })
-        //情况四监听对象写法某个属性
-        watch(()=>{person.a.b},(newval,oldvalue)=>{
-            console.log('prson变化了',newval,oldvalue)
+        //监听
+        watchEffect(()=>{
+            const x = sum.value
+            const x1 = msg.value
+            const x2 = person.age
+            console.log('watcheffect所指向回调执行了')
         })
         return {
             sum,
             msg,
             person,
             changName,
-            chanegAge,
-            listen
+            chanegAge
         }
     }
 }
