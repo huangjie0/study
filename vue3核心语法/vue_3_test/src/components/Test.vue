5023433a327fea8c8148d90e9fcb301e501e03f2
huangjie0 <1364045672@qq.com>
2022-06-17 14:05:21 +0800

    优化

--- vue3核心语法/vue_3_test/src/components/Test.vue
+++ vue3核心语法/vue_3_test/src/components/Test.vue
@@ -6,6 +6,9 @@
       <h2>
           {{msg}}
       </h2>
+      <h2>
+          {{person.a.b}}
+      </h2>
       <button @click="msg+='!'">
           修改信息
       </button>
@@ -20,6 +23,9 @@
       <button @click="chanegAge()">
           增长年龄
       </button>
+      <button @click="listen()">
+          监听对象某个属性
+      </button>
   </div>
 </template>
 
@@ -32,15 +38,18 @@ export default {
         let msg = ref('你好啊')
         let person = reactive({
             name:'张三',
-            age:18
+            age:18,
+            a:{
+                b:1
+            }
         })
         //情况一，监视ref的一个响应式数据
-        watch(sum,(newval,oldval)=>{
-            console.log('sum的值变化了',newval,oldval)
-        },{
-            immediate:true,
-            // deep:true
-        })
+        // watch(sum,(newval,oldval)=>{
+        //     console.log('sum的值变化了',newval,oldval)
+        // },{
+        //     immediate:true,
+        //     // deep:true
+        // })
         //定义两个改变的函数
         function changName(){
             person.name='李四'
@@ -49,20 +58,28 @@ export default {
         function chanegAge(){
             person.age += 1
         }
+        function listen(){
+            person.a.b++
+        }
         //情况二，监视ref的多个响应式数据
         // watch([sum,msg],(newval,oldval)=>{
         //     console.log('sum的值变化了',newval,oldval)
         // })
         //情况三监视对象方法
-        watch(person,(newvalue,oldvalue)=>{
-            console.log('person变化了',newvalue,oldvalue)
+        // watch(person,(newvalue,oldvalue)=>{
+        //     console.log('person变化了',newvalue,oldvalue)
+        // })
+        //情况四监听对象写法某个属性
+        watch(()=>{person.a.b},(newval,oldvalue)=>{
+            console.log('prson变化了',newval,oldvalue)
         })
         return {
             sum,
             msg,
             person,
             changName,
-            chanegAge
+            chanegAge,
+            listen
         }
     }
 }
