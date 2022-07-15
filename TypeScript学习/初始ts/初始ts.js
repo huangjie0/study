418882068a497f49220666caa0593763ccab6022
huangjie0 <1364045672@qq.com>
2022-07-15 09:51:01 +0800

    优化

---
+++ TypeScript学习/初始ts/初始ts.js
@@ -0,0 +1,8 @@
+(function () {
+    //这个str类型是字符串类型
+    function sayHi(str) {
+        return '你好啊' + str;
+    }
+    var text = '小明';
+    console.log(sayHi(text));
+})();
---
+++ TypeScript学习/初始ts/初始ts.ts
@@ -0,0 +1,9 @@
+(()=>{
+    //这个str类型是字符串类型
+        function sayHi(str:string){
+            return '你好啊' + str
+        }
+        let text = '小明'
+        console.log(sayHi(text))
+    }
+)()
