c8905e944fe0bf73c9eabfa548a178dcfb61ea0f
huangjie <1364045672@qq.com>
2022-09-23 22:09:47 +0800

    youhua

--- 微信小程序开发/uni-shop-2/subpkg/search/search.vue
+++ 微信小程序开发/uni-shop-2/subpkg/search/search.vue
@@ -4,7 +4,7 @@
 			<uni-search-bar placeholder="请输入搜索内容" :focus="true" @input="input" :radius="100" cancelButton='none'></uni-search-bar>
 		</view>
 		<!-- 搜索建议列表 -->
-		<view class="sugg-list">
+		<view class="sugg-list" v-if="searchResult.length !== 0">
 			<view class="sugg-item" v-for="(item,index) in searchResult" :key="index" @click="gotoDetail(item)">
 				<view class="goods-name">
 					{{item.goods_name}}
@@ -13,13 +13,13 @@
 			</view>
 		</view>
 		<!-- 搜索历史 -->
-		<view class="history-box">
+		<view class="history-box" v-if="searchResult.length == 0">
 			<view class="history-title">
 				<text>搜索历史</text>
 				<uni-icons type="trash" size="17"></uni-icons>
 			</view>
 			<view class="history-list">
-				<uni-tag :text="item" v-for="(item,i) in historyList" :key="i">
+				<uni-tag :text="item" type="success" v-for="(item,i) in historyList" :key="i">
 					
 				</uni-tag>
 			</view>
@@ -67,6 +67,25 @@
 </script>
 
 <style lang="scss">
+.uni-tag{
+	margin-top: 5px;
+	margin-right: 5px;;
+}
+.history-box{
+	padding: 0 5px;
+	.history-title{
+		display: flex;
+		justify-content: space-between;
+		height: 40px;
+		align-items: center;
+		font-size: 13px;
+		border-bottom: 1px solid #efefefef;
+	}
+	.history-list{
+		display: flex;
+		flex-wrap: wrap;
+	}
+}
 .search-box{
 	background-color: #C00000;
 	position: sticky;
