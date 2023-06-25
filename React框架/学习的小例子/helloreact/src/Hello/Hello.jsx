8d537cdacc9f779ea7b31c7304c58590d45ba5f9
huangjie4 <1364045672@qq.com>
2023-06-25 19:33:54 +0800

    修改

---
+++ React框架/学习的小例子/helloreact/src/Hello/Hello.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import './Hello.css'
+export default class Hello extends Component{
+    render(){
+        return (
+            <div className='demo'>
+                hello，react
+            </div>
+        )
+    }
+}
