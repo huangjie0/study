28b16538afbf6a34afcbb31ae155c2722491ff8a
huangjie4 <1364045672@qq.com>
2023-06-25 19:37:16 +0800

    修改

--- React框架/学习的小例子/helloreact/src/App.js
+++ React框架/学习的小例子/helloreact/src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react'
 import Hello from './Hello/Hello'
+import Welcome from './Welcome/Welcome'
 export default class App extends Component{
     render(){
         return (
             <div>
                 <Hello></Hello>
+                <Welcome></Welcome>
             </div>
         )
     }
