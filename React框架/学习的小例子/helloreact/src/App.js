7fb38a75f5d90a78e6719dbfb083ce71ba5639ce
huangjie4 <1364045672@qq.com>
2023-06-26 08:29:33 +0800

    修改

--- React框架/学习的小例子/helloreact/src/App.js
+++ React框架/学习的小例子/helloreact/src/App.js
@@ -1,13 +1,19 @@
-import React,{Component} from 'react'
-import Hello from './Hello/Hello'
-import Welcome from './Welcome/Welcome'
-export default class App extends Component{
-    render(){
-        return (
-            <div>
-                <Hello></Hello>
-                <Welcome></Welcome>
-            </div>
-        )
-    }
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/Footer";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+    <div className="todo-container">
+      <div className="todo-wrap">
+        <Header/>
+        <List/>
+        <Footer/>
+      </div>
+    </div>
+    )
+  }
 }
--- React框架/学习的小例子/helloreact/src/index.js
+++ React框架/学习的小例子/helloreact/src/index.js
@@ -1,4 +1,10 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-ReactDom.render(<App/>,document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
