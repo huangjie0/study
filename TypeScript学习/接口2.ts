4695733f341870723e9f29b7b02a4531eca71182
huangjie4 <1364045672@qq.com>
2023-07-11 21:57:58 +0800

    修改

--- TypeScript学习/接口2.ts
+++ TypeScript学习/接口2.ts
@@ -1,1 +1,32 @@
-// 对属性，类，函数约束
+//对属性，类，函数约束
+
+// function getInfo(name:string):void{
+//     console.log('hello Word');
+// }
+// getInfo('哈哈哈')
+// 对json约束
+interface info{
+    name:string,
+    age:number
+}
+
+// function getInfo(info:{name:string}):void{
+//     console.log('hello Word');
+// }
+// getInfo({name:'张三'})
+function getInfo(name:info){
+    console.log(name.name);
+    console.log(name.age);
+}
+function getInfo1(info2:info){
+    console.log(info2.age);
+    console.log(info2.name);
+}
+//必须包含
+var info = {
+    name:'你好',
+    age:20,
+    label:'hh'
+}
+getInfo(info)
+getInfo1(info)
